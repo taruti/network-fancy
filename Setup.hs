40622c3040620eb31c0f3933db4a77ba444edaa2
Taru Karttunen <taruti@taruti.net>
2009-07-31 10:59:20 +0300

    Run test with Setup test instead of building it by default

--- Setup.hs
+++ Setup.hs
@@ -1,3 +1,12 @@
 #!/usr/bin/env runhaskell
 import Distribution.Simple
-main = defaultMain
+import System.Cmd
+import System.Directory
+
+main = defaultMainWithHooks simpleUserHooks{ runTests = \_ _ _ _ -> rt }
+
+rt :: IO ()
+rt = do system "ghc --make -threaded test.hs -o test"
+        system "./test"
+        mapM_ removeFile ["test","test.hi","test.o"]
+        return ()
