2b328e338f5120eec276a895629c1a1a48f6f881
Taru Karttunen <taruti@taruti.net>
2014-12-12 15:23:32 +0200

    modify Setup.hs to keep nix happy, closes #4

--- Setup.hs
+++ Setup.hs
@@ -1,12 +1,4 @@
 #!/usr/bin/env runhaskell
 import Distribution.Simple
-import System.Cmd
-import System.Directory
 
-main = defaultMainWithHooks simpleUserHooks{ runTests = \_ _ _ _ -> rt }
-
-rt :: IO ()
-rt = do system "ghc --make -threaded test.hs -o test"
-        system "./test"
-        mapM_ removeFile ["test","test.hi","test.o"]
-        return ()
+main = defaultMain
